18b641ee733d0993394f89b27a9b9fb6b1e67a3c
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-04-12 16:31:29 +0000

    made changes

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Context } from "../store/appContext"
 
 export const Home = () => {
 	const {store, actions} = useContext(Context)
-
-	console.log(actions.fetch)
+	
+	console.log(store)
 	return (
 	<div>
-		{actions.loadSomeData()}
+		{store.data.results && store.data.results.map((element, index) => {
+			return(
+				<div>{element.name}</div>
+			)
+		})}
 	</div>
-	)
-};
+	)}
+
